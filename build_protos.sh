e4dd265338df673caa81536d00b8176b9a7591e8
Алексей <tatarrr95@gmail.com>
2026-01-16 06:00:31 +0300

    fix: исправлен sed для совместимости с Linux (Docker)

--- build_protos.sh
+++ build_protos.sh
@@ -13,9 +13,9 @@ python -m grpc_tools.protoc \
     "$PROTO_DIR/recognitionv2.proto" \
     "$PROTO_DIR/synthesisv2.proto"
 
-# Исправляем импорты для работы как пакета
-sed -i '' 's/^import recognitionv2_pb2/from . import recognitionv2_pb2/' "$OUT_DIR/recognitionv2_pb2_grpc.py"
-sed -i '' 's/^import synthesisv2_pb2/from . import synthesisv2_pb2/' "$OUT_DIR/synthesisv2_pb2_grpc.py"
+# Исправляем импорты для работы как пакета (совместимо с Linux и macOS)
+sed 's/^import recognitionv2_pb2/from . import recognitionv2_pb2/' "$OUT_DIR/recognitionv2_pb2_grpc.py" > "$OUT_DIR/recognitionv2_pb2_grpc.py.tmp" && mv "$OUT_DIR/recognitionv2_pb2_grpc.py.tmp" "$OUT_DIR/recognitionv2_pb2_grpc.py"
+sed 's/^import synthesisv2_pb2/from . import synthesisv2_pb2/' "$OUT_DIR/synthesisv2_pb2_grpc.py" > "$OUT_DIR/synthesisv2_pb2_grpc.py.tmp" && mv "$OUT_DIR/synthesisv2_pb2_grpc.py.tmp" "$OUT_DIR/synthesisv2_pb2_grpc.py"
 
 touch "$OUT_DIR/__init__.py"
 
